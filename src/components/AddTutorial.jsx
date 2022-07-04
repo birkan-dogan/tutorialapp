783fb39f9ca01665da24274cb1c2cab987a06bf7
birkan <b.561birkan@gmail.com>
2022-07-04 15:15:15 +0300

    AddTutorial comp was created by using bootstrap

--- src/components/AddTutorial.jsx
+++ src/components/AddTutorial.jsx
@@ -1,4 +1,43 @@
+import { useState } from "react";
+
 const AddTutorial = () => {
-  return <div></div>;
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  return (
+    <div className="container text-center mt-4">
+      <h1 className="display-6 text-primary">Add Your Tutorial</h1>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            placeholder="Enter your title"
+            value={title}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Description
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="description"
+            placeholder="Enter Your Description"
+            value={desc}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 export default AddTutorial;
